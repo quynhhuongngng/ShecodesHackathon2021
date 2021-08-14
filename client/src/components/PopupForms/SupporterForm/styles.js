c8181eb64c02a5695585cde6f1f0f66d99c8eadf
nxmai <nxmai.it@gmail.com>
2021-08-14 18:26:06 +0700

    fix popup form

--- client/src/components/PopupForms/SupporterForm/styles.js
+++ client/src/components/PopupForms/SupporterForm/styles.js
@@ -2,16 +2,16 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles((theme) => ({
   paper: {
-    marginTop: theme.spacing(8),
+    marginTop: theme.spacing(2),
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
-    boxShadow: '2px 2px 8px 5px rgba(0, 0, 0, .1)',
-    borderRadius: '10px',
-    padding: theme.spacing(3),
-    backgroundColor: 'white',
-    height: '80vh',
-    overflowY: 'scroll',
+    // boxShadow: '2px 2px 8px 5px rgba(0, 0, 0, .1)',
+    // borderRadius: '10px',
+    padding: theme.spacing(2),
+    // backgroundColor: 'white',
+    // height: '80vh',
+    // overflowY: 'scroll',
   },
   avatar: {
     margin: theme.spacing(1),
--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -2,7 +2,7 @@
 /* eslint-disable react/no-array-index-key */
 /* eslint-disable no-unused-vars */
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react';
+import React, { useState } from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
@@ -12,14 +12,36 @@ import Link from '@material-ui/core/Link';
 import Container from '@material-ui/core/Container';
 import CallIcon from '@material-ui/icons/Call';
 import SearchIcon from '@material-ui/icons/Search';
-import { InputBase } from '@material-ui/core';
+import { Dialog, InputBase } from '@material-ui/core';
 import SupporterCard from '../../components/Card/SupporterCard/SupporterCard';
 import ReceiverCard from '../../components/Card/ReceiverCard/ReceiverCard';
+import SupporterForm from '../../components/PopupForms/SupporterForm/SupporterForm';
+import ReceiverForm from '../../components/PopupForms/ReceiverForm/ReceiverForm';
 
 import useStyles from './styles';
 
-export default function Pricing() {
+export default function HomePage() {
   const classes = useStyles();
+
+  const [openSupport, setOpenSupport] = useState(false);
+  const [openReceive, setOpenReceive] = useState(false);
+
+  const handleClickOpenSupport = () => {
+    setOpenSupport(true);
+  };
+
+  const handleCloseSupport = () => {
+    setOpenSupport(false);
+  };
+
+  const handleClickOpenReceive = () => {
+    setOpenReceive(true);
+  };
+
+  const handleCloseReceive = () => {
+    setOpenReceive(false);
+  };
+
   const supporterForm = [
     {
       vegetables: 1,
@@ -118,10 +140,10 @@ export default function Pricing() {
               <CallIcon />
               <Typography styles={{ marginLeft: '40px' }}>Hotline: 0123 4567</Typography>
             </div>
-            <Button>
+            <Button onClick={handleClickOpenSupport}>
               Muốn cho
             </Button>
-            <Button>
+            <Button onClick={handleClickOpenReceive}>
               Muốn nhận
             </Button>
             <Button href="#" color="primary" variant="outlined" className={classes.volunteer}>
@@ -147,6 +169,13 @@ export default function Pricing() {
           </Grid>
         </Grid>
       </Container>
+
+      <Dialog open={openSupport} onClose={handleCloseSupport}>
+        <SupporterForm />
+      </Dialog>
+      <Dialog open={openReceive} onClose={handleCloseReceive}>
+        <ReceiverForm />
+      </Dialog>
     </>
   );
 }
